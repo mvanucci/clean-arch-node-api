014ecbe15c2d420aefeba7ae7f101dfbcd38df96
Murilo Vanucci <murilo_vanucci@hotmail.com>
2020-12-16 13:51:05 -0300

    Ensure LoginRouter returns if no email is provides

--- login.js
+++ login.js
@@ -1,31 +1,56 @@
-
-module.exports = () => {
-  router.post('/sinup', new SignUpRouter().router)
-}
-
 const express = require('express')
 const router = express.Router()
 
+module.exports = () => {
+  const router = new SignUpRouter()
+  router.post('/sinup', ExpressRouterAdapter.adapt(router))
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+
+// Layer presentation
 // sinup-router
 class SignUpRouter {
-  async route (req, res) {
-    const { email, password, repeatPassword } = req.body
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
     if (password === repeatPassword) {
       new SignUpUseCase().signUp(email, password, repeatPassword)
     }
-    res.status(400).json({ error: ' password must be equal to repeatPassword' })
+
+    return {
+      statusCode: 200,
+      body: user
+    }
   }
 }
 
 const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
-
+// Domain
 // sinup-use case
 class SignUpUseCase {
   async signUp (email, password, repeatPassword) {
     if (password === repeatPassword) {
-      const user = await AccountModel.create({ email, password })
-      return user
+      new AddAccountRepository().add(email, password)
     }
+  }
+}
+
+// Infra
+// add-account-repo
+class AddAccountRepository {
+  async add (email, password, repeatPassword) {
+    const user = await AccountModel.create({ email, password })
+    return user
   }
 }
---
+++ presentation/routers/login-router.spec.js
@@ -0,0 +1,23 @@
+class LoginRouter {
+  router (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: '',
+        password: 'any_passowrd'
+      }
+    }
+    const httpResponse = sut.router(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
